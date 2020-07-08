f69f0ff9afbba0fa847c4b6dde6d1c062b705809
suntod <67999195+suntod@users.noreply.github.com>
2020-07-08 14:48:33 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,7 +17,9 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			//$text = $event['source']['userId'];
+			$text = 'Hello Pimchanok';
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
