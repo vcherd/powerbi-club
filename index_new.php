b749dfbc6f4c25ac93c6a3a2968b7ba62d6b111b
suntod <67999195+suntod@users.noreply.github.com>
2020-07-08 15:29:19 +0700

    Update index_new.php

--- index_new.php
+++ index_new.php
@@ -1,2 +1,7 @@
 <?php
 echo "Hello LINE BOT";
+
+require "vendor/autoload.php";
+$access_token = 'IMvRmZtD1nQRW2USHNt//gp3gFBzk5EzRacgthNSv4vCspPSKw0glEq9+URuU0m4ALq2X4CsHufXuE+jvHiVb+s+DPZaSR/HlkUnW+sJrrX1Tk5K3FfXSpz41eY4lefXGNSBiITS2i0rlspqVZ0zRgdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '55ccde8729536a6df0e0dfca954ef261';
+$idPush = 'U508e825223e51da193359f03da202555';
