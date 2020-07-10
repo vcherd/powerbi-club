95947bad68f0ea9ad1094eb20a06a74487d383b6
suntod <67999195+suntod@users.noreply.github.com>
2020-07-10 17:03:20 +0700

    Update reply_new.php

--- reply_new.php
+++ reply_new.php
@@ -4,7 +4,8 @@ $access_token = 'Rby2d2EQ+lCsIXNHUPVcA8SrY1M6ZSBp3D51L50l32LNC4cuR98xnDhr7x0LQcj
 $channelSecret = '55ccde8729536a6df0e0dfca954ef261';
 $idPush = 'U508e825223e51da193359f03da202555';
 
-$userfile = "vendor/userlist.txt"
+$userfile = "vendor/userlist.txt";
+
 foreach (file($userfile) as $userrec) {
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
