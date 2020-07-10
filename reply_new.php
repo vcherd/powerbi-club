134ad8a9e989b54b335f23420fc65e1f22ad7834
suntod <67999195+suntod@users.noreply.github.com>
2020-07-10 17:01:01 +0700

    Update reply_new.php

--- reply_new.php
+++ reply_new.php
@@ -1,12 +1,15 @@
 <?php
 require "vendor/autoload.php";
-$access_token = 'IMvRmZtD1nQRW2USHNt//gp3gFBzk5EzRacgthNSv4vCspPSKw0glEq9+URuU0m4ALq2X4CsHufXuE+jvHiVb+s+DPZaSR/HlkUnW+sJrrX1Tk5K3FfXSpz41eY4lefXGNSBiITS2i0rlspqVZ0zRgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Rby2d2EQ+lCsIXNHUPVcA8SrY1M6ZSBp3D51L50l32LNC4cuR98xnDhr7x0LQcjiALq2X4CsHufXuE+jvHiVb+s+DPZaSR/HlkUnW+sJrrUAvLIjTzpfS7u1i8wa6T0QKsSMF2yKXBPlKPJIOHaacQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '55ccde8729536a6df0e0dfca954ef261';
 $idPush = 'U508e825223e51da193359f03da202555';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
+$userfile = "vendor/userlist.txt"
+foreach (file($userfile) as $userrec) {
+	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($userrec);
+	$response = $bot->pushMessage($idPush, $textMessageBuilder);
+}
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
