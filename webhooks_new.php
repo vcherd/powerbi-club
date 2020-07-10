0e24eec3d942a75c73b6ec6d27000d8d066c8b14
suntod <67999195+suntod@users.noreply.github.com>
2020-07-10 14:56:48 +0700

    Update webhooks_new.php

--- webhooks_new.php
+++ webhooks_new.php
@@ -27,14 +27,19 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
+			if (str_contains($event['message']['text'], 'register') {
+				// Get text sent
+				
+				//$text = $event['source']['userId'];
+				//$text = 'Hello Pimchanok';
+				file_put_contents($txt_file, $event['source']['userId'] . "\n", FILE_APPEND);
+				
+				$array = file($txt_file);
+				$text = join("",$array);
+			}
+			else {
 			
-			//$text = $event['source']['userId'];
-			//$text = 'Hello Pimchanok';
-			file_put_contents($txt_file, $event['source']['userId'] . "\n", FILE_APPEND);
-			
-			$array = file($txt_file);
-			$text = join("",$array);
+			}
 			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
@@ -64,6 +69,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			
 		}
 	}
 }
