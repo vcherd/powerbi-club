f0a5ef00dd08305144db4f569cb9f0ab6e5494e9
suntod <67999195+suntod@users.noreply.github.com>
2020-07-10 10:06:58 +0700

    Update webhooks_new.php

--- webhooks_new.php
+++ webhooks_new.php
@@ -10,7 +10,8 @@ $access_token = 'Rby2d2EQ+lCsIXNHUPVcA8SrY1M6ZSBp3D51L50l32LNC4cuR98xnDhr7x0LQcj
 $content = file_get_contents('php://input');
 
 // write in log file
-file_put_contents('log.txt', $content . PHP_EOL, FILE_APPEND);
+$log_file = 'log.txt';
+file_put_contents($log_file, $content . PHP_EOL, FILE_APPEND);
 
 // Parse JSON
 $events = json_decode($content, true);
