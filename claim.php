f377120f2ab505d68c6dcb76310a04c8384d06c2
suntod <67999195+suntod@users.noreply.github.com>
2020-07-12 05:42:18 +0700

    Update claim.php

--- claim.php
+++ claim.php
@@ -8,70 +8,32 @@ $channelSecret = '55ccde8729536a6df0e0dfca954ef261';
 
 // Get POST body content
 //$content = file_get_contents('php://input');
-echo "<HTML><BODY>" . $_GET['ec'] . "</BODY></HTML>";
+//echo "<HTML><BODY>" . $_GET['ec'] . "</BODY></HTML>";
+$eCoupon = $_GET['ec'];
 
 $userfile = "vendor/userlist.txt";
 $promotionfile = "vendor/promotion.txt";
-/*
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			
-			// Get replyToken
-			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
-}
-*/
-/*
-foreach(file($userfile) as $userrec) {
-	$userdb = strtok($userrec,"|");
-	$idPush = strtok("|");
+foreach(file($promotionfile) as $promo) {
+	$userdb_promo = strtok($promo,"|");
+	$promo_code = strtok("|");
+	$promo_detail = strtok("|");
 	
-	foreach(file($promotionfile) as $promo) {
-		$userdb_promo = strtok($promo,"|");
-		$promo_code = strtok("|");
-		$promo_detail = strtok("|");
-		
-		if ($userdb == $userdb_promo) {
-			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($promo_detail);
-			$response = $bot->pushMessage($idPush, $textMessageBuilder);
+	if ($eCoupon == $promo_code) {
+		// get user id
+		foreach(file($userfile) as $userrec) {
+			$userdb = strtok($userrec,"|");
+			if ($userdb == $userdb_promo) $idPush = strtok("|");
 		}
+		//reply to user
+		
+		
+		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("Claimed successfully");
+		$response = $bot->pushMessage($idPush, $textMessageBuilder);
 	}
 }
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-*/
