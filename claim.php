692c0df50f8af0e0ef45cca2d3f74c9fe25bcf5f
suntod <67999195+suntod@users.noreply.github.com>
2020-07-13 08:20:55 +0700

    Update claim.php

--- claim.php
+++ claim.php
@@ -1,6 +1,7 @@
 <?php
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+date_default_timezone_set('Asia/Bangkok');
 
 $access_token = 'Rby2d2EQ+lCsIXNHUPVcA8SrY1M6ZSBp3D51L50l32LNC4cuR98xnDhr7x0LQcjiALq2X4CsHufXuE+jvHiVb+s+DPZaSR/HlkUnW+sJrrUAvLIjTzpfS7u1i8wa6T0QKsSMF2yKXBPlKPJIOHaacQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '55ccde8729536a6df0e0dfca954ef261';
