07460600be1322d187253a4aea5af75254b18868
suntod <67999195+suntod@users.noreply.github.com>
2020-07-14 10:52:59 +0700

    Update line_notify.php

--- line_notify.php
+++ line_notify.php
@@ -34,9 +34,7 @@ $data = array(
 รักษาตัว : '.$js_array['Hospitalized'].' คน
 เวลาล่าสุด : '.$js_array['UpdateDate'].'' );
 
-echo "<HTML><BODY>999</Body></html>";
-echo phpinfo();
-/* 
+
 $ch = curl_init();
 curl_setopt( $ch, CURLOPT_URL, $notifyURL);
 curl_setopt( $ch, CURLOPT_POST, 1);
@@ -46,6 +44,10 @@ curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2);
 curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 1); 
 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+echo "<HTML><BODY>000</Body></html>";
+echo phpinfo();
+/* 
 $result = curl_exec( $ch );
 curl_close( $ch );
  
