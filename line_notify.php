d7ae02eb194bd153048268fd66a213a068417d75
suntod <67999195+suntod@users.noreply.github.com>
2020-07-14 09:43:33 +0700

    Update line_notify.php

--- line_notify.php
+++ line_notify.php
@@ -1,6 +1,35 @@
 <?php
 define('LINE_API',”https://notify-api.line.me/api/notify");
 
+require "vendor/autoload.php";
+$access_token = 'Rby2d2EQ+lCsIXNHUPVcA8SrY1M6ZSBp3D51L50l32LNC4cuR98xnDhr7x0LQcjiALq2X4CsHufXuE+jvHiVb+s+DPZaSR/HlkUnW+sJrrUAvLIjTzpfS7u1i8wa6T0QKsSMF2yKXBPlKPJIOHaacQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '55ccde8729536a6df0e0dfca954ef261';
+//$idPush = 'U508e825223e51da193359f03da202555';
+
+$userfile = "vendor/userlist.txt";
+$promotionfile = "vendor/promotion.txt";
+
+foreach(file($userfile) as $userrec) {
+	$userdb = strtok($userrec,"|");
+	$idPush = strtok("|");
+	
+	foreach(file($promotionfile) as $promo) {
+		$userdb_promo = strtok($promo,"|");
+		$promo_code = strtok("|");
+		$promo_detail = strtok("|");
+		
+		$promo_detail = $promo_detail . "\nClick here to claim: https://stark-mountain-69352.herokuapp.com/claim.php?ec=" . $promo_code;
+		
+		if ($userdb == $userdb_promo) {
+			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($promo_detail);
+			$response = $bot->pushMessage($idPush, $textMessageBuilder);
+		}
+	}
+}
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 /*
  
 $token = "HHAwXfTK32lHD8V620krk2aFixxUksd0NPNCIFu2mn0" //ใส่Token ที่copy เอาไว้
@@ -26,4 +55,4 @@ function notify_message($message,$token){
  return $res;
 }
 */
-echo "<HTML><BODY>555</BODY></HTML>";
+//echo "<HTML><BODY>555</BODY></HTML>";
