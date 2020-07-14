47895ca7d965f1ec59d3203cb2d43f92833ef65c
suntod <67999195+suntod@users.noreply.github.com>
2020-07-14 09:23:29 +0700

    Update test_line_notify.php

--- test_line_notify.php
+++ test_line_notify.php
@@ -1,27 +1,7 @@
 <?php
-define('LINE_API',”https://notify-api.line.me/api/notify");
-/* 
-$token = “XZ8kt6CYmksPO5VFJHcHlrcEBCLEfhviwPpvrGLceYU”; 
-$str = “ทดสอบ line notify”;
- 
-$res = notify_message($str,$token);
+require "vendor/autoload.php";
+$access_token = 'Rby2d2EQ+lCsIXNHUPVcA8SrY1M6ZSBp3D51L50l32LNC4cuR98xnDhr7x0LQcjiALq2X4CsHufXuE+jvHiVb+s+DPZaSR/HlkUnW+sJrrUAvLIjTzpfS7u1i8wa6T0QKsSMF2yKXBPlKPJIOHaacQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '55ccde8729536a6df0e0dfca954ef261';
+//$idPush = 'U508e825223e51da193359f03da202555';
 
-print_r($res);
-*/
-function notify_message($message,$token){
- $queryData = array(‘message’ => $message);
- $queryData = http_build_query($queryData,’’,’&’);
- $headerOptions = array( 
-         ‘http’=>array(
-            ‘method’=>’POST’,
-            ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
-                      .”Authorization: Bearer “.$token.”\r\n”
-                      .”Content-Length: “.strlen($queryData).”\r\n”,
-            ‘content’ => $queryData
-         ),
- );
- $context = stream_context_create($headerOptions);
- $result = file_get_contents(LINE_API,FALSE,$context);
- $res = json_decode($result);
- return $res;
-}
+echo "<HTML><BODY>Test</BODY><HTML>
