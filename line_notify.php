647da19b762912af9e6a1310e6ab27f3f0b6e252
suntod <67999195+suntod@users.noreply.github.com>
2020-07-14 09:41:27 +0700

    Update line_notify.php

--- line_notify.php
+++ line_notify.php
@@ -1,10 +1,7 @@
 <?php
 define('LINE_API',”https://notify-api.line.me/api/notify");
 
-require "vendor/autoload.php";
-$access_token = 'Rby2d2EQ+lCsIXNHUPVcA8SrY1M6ZSBp3D51L50l32LNC4cuR98xnDhr7x0LQcjiALq2X4CsHufXuE+jvHiVb+s+DPZaSR/HlkUnW+sJrrUAvLIjTzpfS7u1i8wa6T0QKsSMF2yKXBPlKPJIOHaacQdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '55ccde8729536a6df0e0dfca954ef261';
-//$idPush = 'U508e825223e51da193359f03da202555';
+/*
  
 $token = "HHAwXfTK32lHD8V620krk2aFixxUksd0NPNCIFu2mn0" //ใส่Token ที่copy เอาไว้
 $str = “Hello”; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
@@ -28,3 +25,5 @@ function notify_message($message,$token){
  $res = json_decode($result);
  return $res;
 }
+*/
+echo "<HTML><BODY>555</BODY></HTML>";
