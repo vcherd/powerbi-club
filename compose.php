e66a360e32d2af89eb650e476fc7a21ba5f404de
suntod <67999195+suntod@users.noreply.github.com>
2020-07-15 13:08:53 +0700

    Update compose.php

--- compose.php
+++ compose.php
@@ -2,7 +2,7 @@
 <HEAD><TITLE>Test Line Notify</TITLE></HEAD>
 <BODY>
 <Form name="Form1" method="POST" action="line_notify_post.php">
-<Input type="Text" name"Text1">
+<Input type="Text" name"message">
 <input type="submit">
 </Form>
 </BODY>
