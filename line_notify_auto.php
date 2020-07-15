7f3d2d79adae7176597d8ead447987c862828a02
suntod <67999195+suntod@users.noreply.github.com>
2020-07-16 06:46:01 +0700

    Create line_notify_auto.php

---
+++ line_notify_auto.php
@@ -0,0 +1,29 @@
+<?php 
+
+//echo $_POST["name"];
+
+$data = array('message' => "Test"); // enter message here
+
+
+$notifyURL = "https://notify-api.line.me/api/notify";
+$accToken = "99UtKRjmbuxfVSh6bbiUQLtIoonngNvI2ipXhml2rPC";
+$headers = array(
+ 'Content-Type: application/x-www-form-urlencoded',
+ 'Authorization: Bearer '.$accToken
+);
+
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, $notifyURL);
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2); 
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 1); 
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec( $ch );
+curl_close( $ch );
+ 
+var_dump($result);
+$result = json_decode($result,TRUE);
