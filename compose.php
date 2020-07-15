98338077158f014d899c2f81f8000d9da0ebd7fc
suntod <67999195+suntod@users.noreply.github.com>
2020-07-15 12:37:57 +0700

    Create compose.php

---
+++ compose.php
@@ -0,0 +1,9 @@
+<HTML>
+<HEAD><TITLE>Test Line Notify</TITLE></HEAD>
+<BODY>
+<Form name="Form1" method="POST" action="line_notify_post.php">
+<Input type="Text" name"Text1">
+<input type="submit">
+</Form>
+</BODY>
+</HTML>
