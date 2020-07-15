c39b9113a78a82da2f36ab249a3b431d4b6e4c60
suntod <67999195+suntod@users.noreply.github.com>
2020-07-15 15:50:40 +0700

    Update compose.php

--- compose.php
+++ compose.php
@@ -4,12 +4,12 @@
 <form name="form1" action="line_notify_post.php" method="post">
 Message: <input type="text" name="message"><br>
 
-<input type="submit">
+<input type="submit" value="Send">
 </form>
 
 <form name="form2" action="line_notify_post.php" method="post">
 <input type="hidden" name="message" value="covid">
-<input type="submit">
+<input type="submit" value="Covid Status">
 </form>
 </body>
 </html>
