120fc2e135385197913beb35ae30f8e4840cd27c
suntod <67999195+suntod@users.noreply.github.com>
2020-07-15 15:26:41 +0700

    Update line_notify_post.php

--- line_notify_post.php
+++ line_notify_post.php
@@ -1,8 +1,28 @@
-<html>
-<body>
+<?php 
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+//echo $_POST["name"];
 
-</body>
-</html>
+$notifyURL = "https://notify-api.line.me/api/notify";
+$accToken = "WWgojH0PlQInXPJaj53WspD8PuuguBtIsAV7bpB2hzs";
+$headers = array(
+ 'Content-Type: application/x-www-form-urlencoded',
+ 'Authorization: Bearer '.$accToken
+);
+
+$data = array('message' => $_POST["name"]); // enter message here
+
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, $notifyURL);
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2); 
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 1); 
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec( $ch );
+curl_close( $ch );
+ 
+var_dump($result);
+$result = json_decode($result,TRUE);
