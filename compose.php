4cb80023b12228c2443f50ada95bbbd3a9762e2e
suntod <67999195+suntod@users.noreply.github.com>
2020-07-17 15:55:40 +0700

    Update compose.php

--- compose.php
+++ compose.php
@@ -2,8 +2,8 @@
 <body>
 
 <form name="form1" action="line_notify_post.php" method="post">
-Message: <input type="text" name="message" value="test"><br>
-Image URL: <input type="text" name="imageurl" value="https://q-cf.bstatic.com/images/hotel/max1024x768/193/193221143.jpg"><br>
+Message: <input type="text" name="message" value="แจ้งปรับราคาน้ำมัน"><br>
+Image URL: <input type="text" name="imageurl" value="https://obs.line-scdn.net/hOz8959PWDFRdLB9aZ3oLLXkUB3t7eQdUay9cMCBGTzZ9dVJTbidYMidBRzV_JVIAPiVaYGUSSzYucQUCOS4NYSdASmVtcQUGaXFcMSE?requestFileName=yes"><br>
 <input type="submit" value="Send">
 </form>
 
