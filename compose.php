ac489b7d0e55141f251e5723a263b389a7ca516b
suntod <67999195+suntod@users.noreply.github.com>
2020-07-17 15:51:01 +0700

    Update compose.php

--- compose.php
+++ compose.php
@@ -2,8 +2,8 @@
 <body>
 
 <form name="form1" action="line_notify_post.php" method="post">
-Message: <input type="text" name="message"><br>
-Image URL: <input type="text" name="imageurl"><br>
+Message: <input type="text" name="message" value="test"><br>
+Image URL: <input type="text" name="imageurl" value="https://q-cf.bstatic.com/images/hotel/max1024x768/193/193221143.jpg"><br>
 <input type="submit" value="Send">
 </form>
 
