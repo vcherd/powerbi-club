ca4ba4c239491cfa5cf91d9d56fd526dea11b640
suntod <67999195+suntod@users.noreply.github.com>
2020-08-03 08:44:36 +0700

    Create testjson.php

---
+++ testjson.php
@@ -0,0 +1,11 @@
+<?php
+$url="https://covid19.th-stat.com/api/open/today";
+$contents = file_get_contents($url); 
+$contents = utf8_encode($contents); 
+$results = json_decode($contents); 
+foreach ($results as $key => $value) { 
+    echo "<h2>$key</h2>";
+    foreach ($value as $k => $v) { 
+        echo "$k | $v <br />"; 
+    }
+}
