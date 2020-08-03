4bc20ff82e56d0766b3ecfc4684b49c17ba4340c
suntod <67999195+suntod@users.noreply.github.com>
2020-08-03 08:50:16 +0700

    Update testjson.php

--- testjson.php
+++ testjson.php
@@ -3,9 +3,13 @@ $url="https://covid19.th-stat.com/api/open/today";
 $contents = file_get_contents($url); 
 $contents = utf8_encode($contents); 
 $results = json_decode($contents); 
+
+print_r($results);
+/*
 foreach ($results as $key => $value) { 
     echo "<h2>$key</h2>";
     foreach ($value as $k => $v) { 
         echo "$k | $v <br />"; 
     }
 }
+*/
