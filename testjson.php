dc80afe8d9f43ea4c2d6a1eb72fc5b8c56390933
suntod <67999195+suntod@users.noreply.github.com>
2020-08-03 08:52:52 +0700

    Update testjson.php

--- testjson.php
+++ testjson.php
@@ -4,12 +4,11 @@ $contents = file_get_contents($url);
 $contents = utf8_encode($contents); 
 $results = json_decode($contents); 
 
-print_r($results);
-/*
+//print_r($results);
+
 foreach ($results as $key => $value) { 
-    echo "<h2>$key</h2>";
-    foreach ($value as $k => $v) { 
-        echo "$k | $v <br />"; 
-    }
+echo "<h2>$key | $value</h2>";
+    //foreach ($value as $k => $v) { 
+    //    echo "$k | $v <br />"; 
+    //}
 }
-*/
