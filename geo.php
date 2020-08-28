958ae05c3631f20d7e6720e87c62e11250302d46
suntod <67999195+suntod@users.noreply.github.com>
2020-08-28 15:58:54 +0700

    Update geo.php

--- geo.php
+++ geo.php
@@ -5,11 +5,34 @@
 <title>Get Visitor's Location Using HTML5 Geolocation</title>
 <script>
     function showPosition() {
+        
+        var arr_Destination = [
+            {title:'MTower',lat:13.694827,lng:100.606188},
+            {title:'Paragon',lat:13.7477777029,lng:100.534815},
+        /*  {title:'Place C',lat:ddddd,lng:ddddd},
+            {title:'Place D',lat:ddddd,lng:ddddd},
+            {title:'Place E',lat:ddddd,lng:ddddd},
+            {title:'Place F',lat:ddddd,lng:ddddd},*/
+        ];
+
         if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
                 document.getElementById("result").innerHTML = positionInfo;
+
+                // คำนวนระยะห่าง
+                    for( i = 0;i<arr_Destination.length;i++){    
+                        var htmlTr = '<tr><td>'+arr_Destination[i].title+'</td><td></td></tr>';
+                        //$("#placeData").append(htmlTr);
+    
+                        //posPlace = new GGM.LatLng(arr_Destination[i].lat,arr_Destination[i].lng);     
+                        //destinations.push(posPlace);
+    
+                        
+                    });         
+                }  
             });
+
         } else {
             alert("Sorry, your browser does not support HTML5 geolocation.");
         }
