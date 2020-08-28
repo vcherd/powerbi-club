4ad685f1b67b88af1fb4a544c71420552a8bd679
suntod <67999195+suntod@users.noreply.github.com>
2020-08-28 15:09:50 +0700

    Update geo.php

--- geo.php
+++ geo.php
@@ -1,23 +1,25 @@
-<html>
-<body>
-<p id="demo">Click the button to get your coordinates:</p>
-<button onclick="getLocation()">Try It</button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Get Visitor's Location Using HTML5 Geolocation</title>
 <script>
-var x=document.getElementById("demo");
-function getLocation()
-  {  if (navigator.geolocation)    {    navigator.geolocation.getCurrentPosition(showPosition,showError);    }
-  else{x.innerHTML="Geolocation is not supported by this browser.";}  }
-function showPosition(position)  {  x.innerHTML="Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;     }
-function showError(error)
-  {  switch(error.code)     {    case error.PERMISSION_DENIED:
-      x.innerHTML="User denied the request for Geolocation."
-      break;     case error.POSITION_UNAVAILABLE:
-      x.innerHTML="Location information is unavailable."
-      break;     case error.TIMEOUT:
-      x.innerHTML="The request to get user location timed out."
-      break;     case error.UNKNOWN_ERROR:
-      x.innerHTML="An unknown error occurred."       break;    }  }
+    function showPosition() {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
+                document.getElementById("result").innerHTML = positionInfo;
+            });
+        } else {
+            alert("Sorry, your browser does not support HTML5 geolocation.");
+        }
+    }
 </script>
+</head>
+<body>
+    <div id="result">
+        <!--Position information will be inserted here-->
+    </div>
+    <button type="button" onclick="showPosition();">Show Position</button>
 </body>
 </html>
