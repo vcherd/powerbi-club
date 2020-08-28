a3bde77929c0e8ee2f824c3d235757f1a62397ab
suntod <67999195+suntod@users.noreply.github.com>
2020-08-28 11:54:31 +0700

    Create pic.php

---
+++ imgsrc/photos/pic.php
@@ -0,0 +1,18 @@
+<?php
+// กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+// include composer autoload
+require_once '../../vendor/autoload.php';
+// กรณีมีการเชื่อมต่อกับฐานข้อมูล
+//require_once("dbconnect.php");
+ 
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManager;  
+ 
+// ทดสอบแสดงค่า ตัวแปร $_GET ที่ส่งผ่าน url
+echo "<pre>";
+print_r($_GET);
+?>
