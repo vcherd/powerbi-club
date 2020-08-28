d7d37353cebcd25e11cbb6935d71a472e6905244
suntod <67999195+suntod@users.noreply.github.com>
2020-08-28 13:28:30 +0700

    Update pic.php

--- imgsrc/photos/pic.php
+++ imgsrc/photos/pic.php
@@ -10,7 +10,8 @@ require_once '../../vendor/autoload.php';
 //require_once("dbconnect.php");
  
 // import the Intervention Image Manager Class
-use Intervention\Image\ImageManager;  
+//use Intervention\Image\ImageManager;  
+use Intervention\Image\ImageServiceProvider;  
  
 if(isset($_GET['file']) && $_GET['file']!=""){
     $picFile = trim($_GET['file']);
