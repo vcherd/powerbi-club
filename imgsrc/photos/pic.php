8e5bdf40a25ec0d121f33e7b7adcd254b3ef183b
suntod <67999195+suntod@users.noreply.github.com>
2020-08-28 12:03:41 +0700

    Update pic.php

--- imgsrc/photos/pic.php
+++ imgsrc/photos/pic.php
@@ -10,92 +10,9 @@ require_once '../../vendor/autoload.php';
 //require_once("dbconnect.php");
  
 // import the Intervention Image Manager Class
-use InterventionImageImageManager;  
+use Intervention\Image\ImageManager;  
  
-if(isset($_GET['file']) && $_GET['file']!=""){
-    $picFile = trim($_GET['file']);
-    $originalFilePath = '../../uploadimage/'; // แก้ไขเป็นโฟลเดอร์รูปต้นฉบับ
-    $fullFilePath = $originalFilePath.$picFile;
-    $fullFilePathJPG = $fullFilePath.'.jpg';
-    $fullFilePathPNG = $fullFilePath.'.png';
-    $fullFile = '';
-    $picType = '';
-    if(file_exists($fullFilePathJPG)){
-        $picType = 'jpg';
-        $fullFile = $fullFilePath.'.'.$picType;
-    }
-    if(file_exists($fullFilePathPNG)){
-        $picType = 'png';
-        $fullFile = $fullFilePath.'.'.$picType;
-    }   
-    if($picType==''){
-        header("HTTP/1.0 404 Not Found");
-        exit;
-    }
-    // สร้างตัวแปรอ้างอิง object ตัวจัดการรูปภาพ
-    $manager = new ImageManager();    
-    $img = $manager->make($fullFile);     
-     
-    $width = (isset($_GET['width']) && $_GET['width']!="")?$_GET['width']:NULL;
-    $height = (isset($_GET['height']) && $_GET['height']!="")?$_GET['height']:NULL;
-    $mode = (isset($_GET['mode']) && $_GET['mode']!="")?$_GET['mode']:NULL;
-     
-    if(!is_null($width) && !is_null($height)){
-        switch ($mode) {
-            case 'f':
-                $img->fit($width, $height, function ($constraint) {
-                    $constraint->upsize(); // ถ้าค่าที่กำหนดมากกว่าค่าเดิม ไม่ต้องปรับขนาด
-                }); 
-                break;  
-            case 'c':
-                $img->crop($width, $height); 
-                break;  
-            case 'r':
-                $img->resize($width, $height);   
-                break;  
-            case 'w':
-                $img->widen($width, function ($constraint) {
-                    $constraint->upsize(); // ถ้าค่าที่กำหนดมากกว่าค่าเดิม ไม่ต้องปรับขนาด
-                }); 
-                break;          
-            case 'h':
-                $img->heighten($height, function ($constraint) {
-                    $constraint->upsize(); // ถ้าค่าที่กำหนดมากกว่าค่าเดิม ไม่ต้องปรับขนาด
-                }); 
-                break;                                                                  
-        }
-    }else{
-        if(!is_null($width)){
-            switch ($mode) {
-                case 'f':
-                    $img->fit($width);   
-                    break;  
-                case 'c':
-                    $img->crop($width, $width);  
-                    break;  
-                case 'r':
-                    $img->resize($width, null, function ($constraint) {
-                        $constraint->aspectRatio();// ให้คงสัดส่วนของรูปภาพ
-                    }); 
-                    break;  
-                case 'w':
-                    $img->widen($width, function ($constraint) {
-                        $constraint->upsize(); // ถ้าค่าที่กำหนดมากกว่าค่าเดิม ไม่ต้องปรับขนาด
-                    }); 
-                    break;          
-                case 'h':
-                    $img->heighten($width, function ($constraint) {
-                        $constraint->upsize(); // ถ้าค่าที่กำหนดมากกว่าค่าเดิม ไม่ต้องปรับขนาด
-                    }); 
-                    break;      
-            }
-        }
-    }
-     
-    // ส่ง HTTP header และข้อมูลของรูปเพื่อนำไปแสดง
-    echo $img->response();           
-}else{
-    header("HTTP/1.0 404 Not Found");
-    exit;   
-}
+// ทดสอบแสดงค่า ตัวแปร $_GET ที่ส่งผ่าน url
+echo "<pre>";
+print_r($_GET);
 ?>
