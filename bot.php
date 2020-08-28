71463b97b4d61af3c6a151266d571ad316c1da7b
suntod <67999195+suntod@users.noreply.github.com>
2020-08-28 10:35:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -354,7 +354,53 @@ if(!is_null($events)){
                             )
                         );              
                         break;                                          
-                    case "p":
+                    case "t_c":
+                            // กำหนด action 4 ปุ่ม 4 ประเภท
+                            $actionBuilder = array(
+                                new MessageTemplateActionBuilder(
+                                    'Message Template',// ข้อความแสดงในปุ่ม
+                                    'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                                ),
+                                new UriTemplateActionBuilder(
+                                    'Uri Template', // ข้อความแสดงในปุ่ม
+                                    'https://www.ninenik.com'
+                                ),
+                                new PostbackTemplateActionBuilder(
+                                    'Postback', // ข้อความแสดงในปุ่ม
+                                    http_build_query(array(
+                                        'action'=>'buy',
+                         
+                                        'item'=>100
+                                    )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                                    'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                                ),      
+                            );
+                            $replyData = new TemplateMessageBuilder('Carousel',
+                                new CarouselTemplateBuilder(
+                                    array(
+                                        new CarouselColumnTemplateBuilder(
+                                            'Title Carousel',
+                                            'Description Carousel',
+                                            'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
+                                            $actionBuilder
+                                        ),
+                                        new CarouselColumnTemplateBuilder(
+                                            'Title Carousel',
+                                            'Description Carousel',
+                                            'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
+                                            $actionBuilder
+                                        ),
+                                        new CarouselColumnTemplateBuilder(
+                                            'Title Carousel',
+                                            'Description Carousel',
+                                            'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
+                                            $actionBuilder
+                                        ),                                          
+                                    )
+                                )
+                            );
+                            break;  
+                        case "p":
                         // ถ้าขณะนั้นเป็นการสนทนาใน ROOM หรือ GROUP
                         if(!is_null($groupId) || !is_null($roomId)){
                             if($eventObj->isGroupEvent()){// ถ้าอยู่ใน GROUP
