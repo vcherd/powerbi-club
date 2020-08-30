01dbecbbbdb504fffa309cab08837afce1be1994
suntod <67999195+suntod@users.noreply.github.com>
2020-08-30 08:38:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -103,7 +103,12 @@ if(!is_null($events)){
                 $userMessage = strtolower($userMessage); // แปลงเป็นตัวเล็ก สำหรับทดสอบ
                 switch ($userMessage) {
                     case "t":
+                        /*
                         $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+                        $replyData = new TextMessageBuilder($textReplyMessage);
+                        break;
+                        */
+                        $textReplyMessage = "สวัสดีครับ คุณ " + $userID;
                         $replyData = new TextMessageBuilder($textReplyMessage);
                         break;
                     case "i":
