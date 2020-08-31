40e2e97f693ab89c97a96f14c7ceb9da90d7c851
suntod <67999195+suntod@users.noreply.github.com>
2020-08-31 10:41:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,6 +9,7 @@ require_once 'vendor/autoload.php';
  
 // การตั้งเกี่ยวกับ bot
 require_once 'config/bot_settings.php';
+require_once 'config/config.php';
  
 // กรณีมีการเชื่อมต่อกับฐานข้อมูล
 //require_once("dbconnect.php");
