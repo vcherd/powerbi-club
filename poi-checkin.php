9b440d1ed9087210db309d5074b0bebfa08503d1
suntod <67999195+suntod@users.noreply.github.com>
2020-08-31 08:38:41 +0700

    Update poi-checkin.php

--- poi-checkin.php
+++ poi-checkin.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'config/config.php';
-/*
+
 $checkinfile = fopen(FILE_CHECK_IN_FULLPATH, "a+") or die("Unable to open file!");
 $txt = $_POST["userID"] . "\t" . $_POST["userLoc"];
 fwrite($checkinfile, $txt);
 fclose($checkinfile);
-*/
+
 echo $_POST["userLoc"] . "<BR>" . $_POST["userID"];
 ?>
