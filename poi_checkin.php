6933e1984e797572a9d6cab7bec431c12462908f
suntod <67999195+suntod@users.noreply.github.com>
2020-08-31 11:07:19 +0700

    Update poi_checkin.php

--- poi_checkin.php
+++ poi_checkin.php
@@ -6,8 +6,17 @@ $txt = date('d-m-Y h:i:s A') . "|" . $_POST["userID"] . "|" . $_POST["userLoc"] 
 fwrite($checkinfile, $txt);
 fclose($checkinfile);
 
-//echo $_POST["userLoc"] . "<BR>" . $_POST["userID"];
-$myfile = fopen(FILE_CHECK_IN_FULLPATH, "r") or die("Unable to open file!");
-echo str_replace("\n","<BR>",fread($myfile,filesize(FILE_CHECK_IN_FULLPATH)));
-fclose($myfile);
+echo "Check-in Success!!";
 ?>
+<script>
+function countdown() {
+    var i = document.getElementById('counter');
+    i.innerHTML = parseInt(i.innerHTML)-1;
+
+    if (parseInt(i.innerHTML)<=0) {
+        window.close();
+    }
+}
+
+setInterval(function(){ countdown(); },300);
+</script>
