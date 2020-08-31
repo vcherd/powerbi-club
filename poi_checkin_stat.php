19a3ce6f91e99fe6b1d04f2219972d9ce43d0ad8
suntod <67999195+suntod@users.noreply.github.com>
2020-08-31 10:26:38 +0700

    Create poi_checkin_stat.php

---
+++ poi_checkin_stat.php
@@ -0,0 +1,13 @@
+<?php
+require_once './config/config.php';
+
+$checkinfile = fopen(FILE_CHECK_IN_FULLPATH, "a+") or die("Unable to open file!");
+$txt = date('d-m-Y h:i:s A') . "|" . $_POST["userID"] . "|" . $_POST["userLoc"] . "\n";
+fwrite($checkinfile, $txt);
+fclose($checkinfile);
+
+//echo $_POST["userLoc"] . "<BR>" . $_POST["userID"];
+$myfile = fopen(FILE_CHECK_IN_FULLPATH, "r") or die("Unable to open file!");
+echo fread($myfile,filesize(FILE_CHECK_IN_FULLPATH));
+fclose($myfile);
+?>
