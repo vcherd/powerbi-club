58469e6b77ec2d2d4fe886d52ef9d434ca0675a5
suntod <67999195+suntod@users.noreply.github.com>
2020-08-31 10:53:45 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,9 @@
 <?php
 date_default_timezone_set('Asia/Bangkok');
 
+//general settings
+define('POI_CHECK_IN_DISTANCE',0.4); //unit: km
+
+//file settings
 define('FILE_CHECK_IN_FULLPATH','./db/user_checkin.txt');
 ?>
