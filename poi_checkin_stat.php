f2ea909014c98989bf2a99951d8ddbeb98697537
suntod <67999195+suntod@users.noreply.github.com>
2020-08-31 10:45:59 +0700

    Update poi_checkin_stat.php

--- poi_checkin_stat.php
+++ poi_checkin_stat.php
@@ -2,6 +2,6 @@
 require_once 'config/config.php';
 
 $myfile = fopen(FILE_CHECK_IN_FULLPATH, "r") or die("Unable to open file!");
-echo fread($myfile,filesize(FILE_CHECK_IN_FULLPATH));
+echo str_replace("\n","<BR>",fread($myfile,filesize(FILE_CHECK_IN_FULLPATH)));
 fclose($myfile);
 ?>
