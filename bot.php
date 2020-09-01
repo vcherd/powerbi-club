e7381e321fc3601237a06eb1e78a72b31022a5a5
suntod <67999195+suntod@users.noreply.github.com>
2020-09-01 11:57:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -331,7 +331,39 @@ if(!is_null($events)){
                                 )
                             )
                         );
-                        break;                                                                                                                                                                                                  
+                        break;  
+                    case "fl":                                                                                                                                                                                                    
+                        $textReplyMessage = new BubbleContainerBuilder(
+                            "ltr",  // กำหนด NULL หรือ "ltr" หรือ "rtl"
+                            new BoxComponentBuilder(
+                                "vertical",
+                                array(
+                                    new TextComponentBuilder("This is Header")
+                                )
+                            ),
+                            new ImageComponentBuilder(
+                                "https://www.ninenik.com/images/ninenik_page_logo.png",NULL,NULL,NULL,NULL,"full","20:13","cover"),
+                            new BoxComponentBuilder(
+                                "vertical",
+                                array(
+                                    new TextComponentBuilder("This is Body")
+                                )
+                            ),
+                            new BoxComponentBuilder(
+                                "vertical",
+                                array(
+                                    new TextComponentBuilder("This is Footer")
+                                )
+                            ),
+                            new BubbleStylesBuilder( // style ทั้งหมดของ bubble
+                                new BlockStyleBuilder("#FFC90E"),  // style สำหรับ header block
+                                new BlockStyleBuilder("#EFE4B0"), // style สำหรับ hero block
+                                new BlockStyleBuilder("#B5E61D"), // style สำหรับ body block
+                                new BlockStyleBuilder("#FFF200") // style สำหรับ footer block
+                            )
+                        );
+                        $replyData = new FlexMessageBuilder("Flex",$textReplyMessage);
+                        break;
                     default:
                         $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
                         $replyData = new TextMessageBuilder($textReplyMessage);         
