da37f245af8f4e30b281fcd08b0688f6188ec249
suntod <67999195+suntod@users.noreply.github.com>
2020-09-01 08:28:31 +0700

    Update poi_checkin.php

--- poi_checkin.php
+++ poi_checkin.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'config/config.php';
 
+if (($_POST["userID"] == "") || ($_POST["userLoc"] == "")) die ("Internal error, missing value.");
+
 $checkinfile = fopen(FILE_CHECK_IN_FULLPATH, "a+") or die("Unable to open file!");
 $txt = date('d-m-Y h:i:s A') . "|" . $_POST["userID"] . "|" . $_POST["userLoc"] . "\n";
 fwrite($checkinfile, $txt);
