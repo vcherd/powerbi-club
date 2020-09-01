66705142a356516b94ecf9187b24e124d99e6c75
suntod <67999195+suntod@users.noreply.github.com>
2020-09-01 15:28:40 +0700

    Update img_thumb.php

--- img_thumb.php
+++ img_thumb.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'config/config.php';
 
+use Gregwar\Image\Image;
+
 $imgfolder = USER_IMAGE_FOLDER . '/' . $_GET["userID"] . '/';
 $thumbfolder = $imgfolder . USER_IMAGE_THUMBNAIL_SUBFOLDER . '/';
 
@@ -20,35 +22,17 @@ function createThumbs( $pathToImages, $pathToThumbs, $thumbWidth )
       //echo "output: " . $pathToThumbs . "<br>";
       echo "Creating thumbnail for {$fname} <br />";
 
-      // load image and get image size
-      $img = imagecreatefromjpeg( "{$pathToImages}{$fname}" );
-      $width = imagesx( $img );
-      $height = imagesy( $img );
-
-      // calculate thumbnail size
-      $new_width = $thumbWidth;
-      $new_height = floor( $height * ( $thumbWidth / $width ) );
-
-      // create a new temporary image
-      $tmp_img = imagecreatetruecolor( $new_width, $new_height );
-
-      // copy and resize old image into new image 
-      imagecopyresized( $tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
-
-      // save thumbnail into a file
-      imagejpeg( $tmp_img, "{$pathToThumbs}{$fname}" );
-
+      Image::open($pathToImages . $fname)
+        ->resize(100, 100)
+        ->negate()
+        ->save($pathToThumbs . $fname);
+      
       echo "{$pathToThumbs}{$fname}";
     }
   }
   // close the directory
   closedir( $dir );
 }
-// call createThumb function and pass to it as parameters the path 
-// to the directory that contains images, the path to the directory
-// in which thumbnails will be placed and the thumbnail's width. 
-// We are assuming that the path will be a relative path working 
-// both in the filesystem, and through the web for links
 
 
 
