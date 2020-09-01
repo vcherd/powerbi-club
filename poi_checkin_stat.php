d2e0d7d6b2147cd1d46ee7dbf63a8ce6cef4e88c
suntod <67999195+suntod@users.noreply.github.com>
2020-09-01 08:43:16 +0700

    Update poi_checkin_stat.php

--- poi_checkin_stat.php
+++ poi_checkin_stat.php
@@ -1,7 +1,21 @@
 <?php
 require_once 'config/config.php';
 
-$myfile = fopen(FILE_CHECK_IN_FULLPATH, "r") or die("Unable to open file!");
-echo str_replace("\n","<BR>",fread($myfile,filesize(FILE_CHECK_IN_FULLPATH)));
+//$myfile = fopen(FILE_CHECK_IN_FULLPATH, "r") or die("Unable to open file!");
+//echo str_replace("\n","<BR>",fread($myfile,filesize(FILE_CHECK_IN_FULLPATH)));
+
+if ($file = fopen(FILE_CHECK_IN_FULLPATH, "r")) {
+    while(!feof($file)) {
+        $line = fgets($file);
+        $checkindatetime = strtok($line,"|");
+        $uid_fromfile = strtok("|");
+        $loc = strtok("|");
+
+        if ($uid_fromfile == $_POST["userID"]) echo $checkindatetime . " at " . $loc . "<BR>";
+    }
+    fclose($file);
+}
+else die("Unable to open file!");
+
 fclose($myfile);
 ?>
