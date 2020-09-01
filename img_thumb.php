5abcd3b89f1678c84b71a0c56dcc3bee32837785
suntod <67999195+suntod@users.noreply.github.com>
2020-09-01 16:48:55 +0700

    Update img_thumb.php

--- img_thumb.php
+++ img_thumb.php
@@ -4,6 +4,9 @@ require_once 'config/config.php';
 $imgfolder = USER_IMAGE_FOLDER . '/' . $_GET["userID"] . '/';
 $thumbfolder = $imgfolder . USER_IMAGE_THUMBNAIL_SUBFOLDER . '/';
 
+echo "source: " . $imgfolder . "<BR>";
+echo "dest: " . &thumbfolder . "<BR>";
+
 function createThumbs( $pathToImages, $pathToThumbs, $thumbWidth ) 
 {
 // Turn off all error reporting
