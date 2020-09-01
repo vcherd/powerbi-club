76bc733e94d1e2de55cdb0ff41786ac9552cb203
suntod <67999195+suntod@users.noreply.github.com>
2020-09-01 16:45:41 +0700

    Update img_thumb.php

--- img_thumb.php
+++ img_thumb.php
@@ -6,34 +6,79 @@ $thumbfolder = $imgfolder . USER_IMAGE_THUMBNAIL_SUBFOLDER . '/';
 
 function createThumbs( $pathToImages, $pathToThumbs, $thumbWidth ) 
 {
+// Turn off all error reporting
+error_reporting(0);
+
+set_time_limit(0);
   // open the directory
   $dir = opendir( $pathToImages );
 
   // loop through it, looking for any/all JPG files:
+  $i='1';
   while (false !== ($fname = readdir( $dir ))) {
     // parse path for the extension
     $info = pathinfo($pathToImages . $fname);
     // continue only if this is a JPEG image
-    if ( strtolower($info['extension']) == 'jpg' ) 
+        $source_file_name = basename($source_image);
+        $source_image_type = substr($source_file_name, -3, 3);
+
+        switch(strtolower($info['extension']))
+        {
+        case 'jpg':
+            $img = imagecreatefromjpeg("{$pathToImages}{$fname}");
+            break;
+
+        case 'gif':
+            $img = imagecreatefromgif("{$pathToImages}{$fname}");
+            break;
+
+        case 'png':
+            $img = imagecreatefrompng("{$pathToImages}{$fname}");
+            break;    
+        }
+
+      echo "$i : Creating thumbnail for small_$fname <br />";
+
+      // load image and get image size
+      $width = imagesx( $img );
+      $height = imagesy( $img );
+
+    // this will be our cropped image
+
+    // copy the crop area from the source image to the blank image created above
+
+    // calculate thumbnail size
+      $new_width = $thumbWidth;
+      $new_height = $thumbWidth;
+
+      // create a new tempopary image
+      $tmp_img = imagecreatetruecolor( $new_width, $new_height );
+
+
+      // copy and resize old image into new image 
+      imagecopyresized( $tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+
+
+    switch(strtolower($info['extension']))
     {
-      //echo "input: " . $pathToImages . $fname . "<br>";
-      //echo "output: " . $pathToThumbs . "<br>";
-      echo "Creating thumbnail for {$fname} <br />";
-    
-      echo "{$pathToThumbs}{$fname}";
+        case 'jpg':
+            imagejpeg($tmp_img, "{$pathToThumbs}small_$fname", 100);
+            break;
+
+        case 'gif':
+            imagegif($tmp_img, "{$pathToThumbs}small_$fname");
+            break;
+
+        case 'png':
+            imagepng($tmp_img,"{$pathToThumbs}small_$fname", 0);
+            break;    
     }
-  }
+    imagedestroy($img);
+    imagedestroy($tmp_img);
+    $i++;
+      }
   // close the directory
   closedir( $dir );
-}
-
-
-
-if(!file_exists($imgfolder)) { // ตรวจสอบถ้ายังไม่มีให้สร้างโฟลเดอร์ userId
-    mkdir($imgfolder, 0777, true);
-}
-if(!file_exists($thumbfolder)) { // ตรวจสอบถ้ายังไม่มีให้สร้างโฟลเดอร์ userId
-    mkdir($thumbfolder, 0777, true);
 }
 
 createThumbs($imgfolder, $thumbfolder,100);
