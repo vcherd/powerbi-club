a0b0dc97cfd1a49a2fa9907da43b79a85f19d3da
suntod <67999195+suntod@users.noreply.github.com>
2020-09-01 13:30:17 +0700

    Update bot_action.php

--- bot_action.php
+++ bot_action.php
@@ -1,4 +1,16 @@
 <?php
+// กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+// include composer autoload
+require_once 'vendor/autoload.php';
+ 
+// การตั้งเกี่ยวกับ bot
+require_once 'config/bot_settings.php';
+require_once 'config/config.php';
+
 ///////////// ส่วนของการเรียกใช้งาน class ผ่าน namespace
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient;
