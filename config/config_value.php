3a471ce5053f9ad1be399d1d8a99171f90d2c88c
suntod <67999195+suntod@users.noreply.github.com>
2020-09-02 19:51:58 +0700

    Create config_value.php

---
+++ config/config_value.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set('Asia/Bangkok');
+
+//general settings
+define('POI_CHECK_IN_DISTANCE',0.2); //unit: km
+
+//file settings
+define('FILE_CHECK_IN_FULLPATH','db/user_checkin.txt');
+define('USER_IMAGE_FOLDER','userimage');
+define('USER_IMAGE_THUMBNAIL_SUBFOLDER','thumbnails');
+?>
