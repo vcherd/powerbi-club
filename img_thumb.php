0a1e7043dd50fd0474b181583610c9ed8256b9fd
suntod <67999195+suntod@users.noreply.github.com>
2020-09-02 16:21:26 +0700

    Update img_thumb.php

--- img_thumb.php
+++ img_thumb.php
@@ -24,7 +24,7 @@ foreach($files1 as $key=>$value){
 //echo "Total = " . $total . "<BR>";
 
 // จำนวนรายการที่ต้องการแสดง แต่ละหน้า
-$perPage = 2;
+$perPage = 10;
  
 // คำนวณจำนวนหน้าทั้งหมด
 $num_naviPage=ceil($total/$perPage);
