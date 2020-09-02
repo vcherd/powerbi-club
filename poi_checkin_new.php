87b3dca489e0414cc66d0fd5b62bd0ba9e9de015
suntod <67999195+suntod@users.noreply.github.com>
2020-09-02 21:42:19 +0700

    Update poi_checkin_new.php

--- poi_checkin_new.php
+++ poi_checkin_new.php
@@ -9,5 +9,21 @@ fwrite($checkinfile, $txt);
 fclose($checkinfile);
 
 echo "Check-in at " . $_POST["userLoc"] . " Success.";
+
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+    $radlat1 = M_PI * $lat1/180;
+    $radlat2 = M_PI * $lat2/180;
+    $radlon1 = M_PI * $lon1/180;
+    $radlon2 = M_PI * $lon2/180;
+    $theta = $lon1-$lon2;
+    $radtheta = M_PI * $theta/180;
+    $dist = sin($radlat1) * sin($radlat2) + cos($radlat1) * cos($radlat2) * cos($radtheta);
+    $dist = acos($dist);
+    $dist = $dist * 180/M_PI;
+    $dist = $dist * 60 * 1.1515;
+    if ($unit=="K") { $dist = $dist * 1.609344; }
+    if ($unit=="N") { $dist = $dist * 0.8684; }
+return $dist;
+}
 ?>
 <script> window.setTimeout("window.close()", 1000); </script>
