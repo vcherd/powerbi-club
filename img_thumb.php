aed21086f69f260cf4146e75e1813bb69e164d31
suntod <67999195+suntod@users.noreply.github.com>
2020-09-02 08:45:53 +0700

    Update img_thumb.php

--- img_thumb.php
+++ img_thumb.php
@@ -4,87 +4,52 @@ require_once 'config/config.php';
 $imgfolder = USER_IMAGE_FOLDER . '/' . $_GET["userID"] . '/';
 $thumbfolder = $imgfolder . USER_IMAGE_THUMBNAIL_SUBFOLDER . '/';
 
-echo "source: " . $imgfolder . "<BR>";
-echo "dest: " . $thumbfolder . "<BR>";
-
-function createThumbs( $pathToImages, $pathToThumbs, $thumbWidth ) 
-{
-// Turn off all error reporting
-error_reporting(0);
-
-set_time_limit(0);
-  // open the directory
-  $dir = opendir( $pathToImages );
-
-  // loop through it, looking for any/all JPG files:
-  $i='1';
-  while (false !== ($fname = readdir( $dir ))) {
-    // parse path for the extension
-    $info = pathinfo($pathToImages . $fname);
-    // continue only if this is a JPEG image
-        $source_file_name = basename($source_image);
-        $source_image_type = substr($source_file_name, -3, 3);
-
-        switch(strtolower($info['extension']))
-        {
-        case 'jpg':
-            $img = imagecreatefromjpeg("{$pathToImages}{$fname}");
-            break;
-
-        case 'gif':
-            $img = imagecreatefromgif("{$pathToImages}{$fname}");
-            break;
-
-        case 'png':
-            $img = imagecreatefrompng("{$pathToImages}{$fname}");
-            break;    
+$allowed_types=array('jpg','jpeg','gif','png');
+$dir    = $imgfolder;
+$files1 = scandir($dir);
+$total=0; // นับจำนวนรูปทั้งหมด
+$pic_path=array();
+foreach($files1 as $key=>$value){
+    if($key>1){
+        $file_parts = explode('.',$value);
+        $ext = strtolower(array_pop($file_parts));
+        if(in_array($ext,$allowed_types)){
+            $pic_path[]=$dir.$value;
+            $total++;
+             
         }
-
-      echo "$i : Creating thumbnail for small_$fname <br />";
-
-      // load image and get image size
-      $width = imagesx( $img );
-      $height = imagesy( $img );
-
-    // this will be our cropped image
-
-    // copy the crop area from the source image to the blank image created above
-
-    // calculate thumbnail size
-      $new_width = $thumbWidth;
-      $new_height = $thumbWidth;
-
-      // create a new tempopary image
-      $tmp_img = imagecreatetruecolor( $new_width, $new_height );
-
-
-      // copy and resize old image into new image 
-      imagecopyresized( $tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
-
-
-    switch(strtolower($info['extension']))
-    {
-        case 'jpg':
-            imagejpeg($tmp_img, "{$pathToThumbs}small_$fname", 100);
-            break;
-
-        case 'gif':
-            imagegif($tmp_img, "{$pathToThumbs}small_$fname");
-            break;
-
-        case 'png':
-            imagepng($tmp_img,"{$pathToThumbs}small_$fname", 0);
-            break;    
+ 
     }
-    imagedestroy($img);
-    imagedestroy($tmp_img);
-    $i++;
-      }
-  // close the directory
-  closedir( $dir );
 }
+// จำนวนรายการที่ต้องการแสดง แต่ละหน้า
+$perPage = 2;
+ 
+// คำนวณจำนวนหน้าทั้งหมด
+$num_naviPage=ceil($total/$perPage);
+ 
+// กำหนดจุดเริ่มต้น และสิ้นสุดของรายการแต่ละหน้าที่จะแสดง
+if(!isset($_GET['page'])){
+    $s_key=0;
+    $e_key=$perPage;    
+    $_GET['page']=1;
+}else{
+    $s_key=($_GET['page']*$perPage)-$perPage;
+    $e_key=$perPage*$_GET['page'];
+    $e_key=($e_key>$total)?$total:$e_key;
+}
+for($i=1;$i<=$num_naviPage;$i++){
+    echo "  || <a href=\"?page=".$i."\">Page $i</a>";   
+}
+echo "<hr>";
+ 
+// แสดงรายการ
+for($indexPicture=$s_key;$indexPicture<$e_key;$indexPicture++){
+ 
+        echo "<img style='width:100px;' src='".$pic_path[$indexPicture]."'/>&nbsp;";  
+}
+ 
+// แสดงหน้าปัจจุบัน
 
-createThumbs($imgfolder, $thumbfolder,100);
 
-echo "success";
+
 ?>
