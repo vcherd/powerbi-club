e9079895cbd3ea378046809e0028d323deb365d2
suntod <67999195+suntod@users.noreply.github.com>
2020-09-03 06:49:50 +0700

    Update config_function.php

--- config/config_function.php
+++ config/config_function.php
@@ -1,7 +1,7 @@
 <?php
 
 function CheckRef($ref_site) {
-    echo "ref = ".$_SERVER['HTTP_REFERER'] . "<BR>ref2=" . $ref_site;
+    //echo "ref = ".$_SERVER['HTTP_REFERER'] . "<BR>ref2=" . $ref_site;
 
     if(strpos($_SERVER['HTTP_REFERER'],$ref_site) == FALSE )die("Error");
     //header('Location:page1.php')
