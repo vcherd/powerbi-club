422eae96632c2b2851ea9f8fac71262e9dad2954
suntod <67999195+suntod@users.noreply.github.com>
2020-09-02 18:56:39 +0700

    Update poi_checkin_stat.php

--- poi_checkin_stat.php
+++ poi_checkin_stat.php
@@ -4,6 +4,8 @@ require_once 'config/config.php';
 //$myfile = fopen(FILE_CHECK_IN_FULLPATH, "r") or die("Unable to open file!");
 //echo str_replace("\n","<BR>",fread($myfile,filesize(FILE_CHECK_IN_FULLPATH)));
 //echo "userid = " . $_GET["userID"];
+$found = false;
+
 if ($file = fopen(FILE_CHECK_IN_FULLPATH, "r")) {
     while(!feof($file)) {
         $line = fgets($file);
@@ -11,11 +13,16 @@ if ($file = fopen(FILE_CHECK_IN_FULLPATH, "r")) {
         $uid_fromfile = strtok("|");
         $loc = strtok("|");
         
-        if ($uid_fromfile == $_GET["userID"]) echo $checkindatetime . " at " . $loc . "<BR>";
+        if ($uid_fromfile == $_GET["userID"]) {
+            echo $checkindatetime . " at " . $loc . "<BR>";
+            $found = true;
+        }
     }
     fclose($file);
 }
 else die("Unable to open file!");
 
 fclose($myfile);
+
+if ($found == false) echo ("No check-in data.");
 ?>
