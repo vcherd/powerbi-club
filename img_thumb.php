9bfde73a15235dd3c067cc2b22dd725ff63dbdd5
suntod <67999195+suntod@users.noreply.github.com>
2020-09-02 13:27:43 +0700

    Update img_thumb.php

--- img_thumb.php
+++ img_thumb.php
@@ -40,7 +40,7 @@ if(!isset($_GET['page'])){
     $e_key=($e_key>$total)?$total:$e_key;
 }
 for($i=1;$i<=$num_naviPage;$i++){
-    echo "  || <a href=\"?page=".$i .'&userID=' . $_GET["userID"] ."\">Page $i</a>";   
+    echo "  || <a href=\"?page=".$i."\">Page $i</a>";   
 }
 echo "<hr>";
  
