c6265c7f404b5a5e313220a0ee1ecc6c82a66d97
suntod <67999195+suntod@users.noreply.github.com>
2020-09-03 05:18:20 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,4 +5,11 @@ require_once './config/config_value.php';
 $verifydata = hash(HASH_ALGORITHM, $_GET["userID"] . SALT);
 
 if ($_GET["sID"] != $verifydata) die ("Error");
+
+function CheckRef($ref_site) {
+    if($_SERVER['HTTP_REFERER'] != $ref_site) die("Error");
+    //header('Location:page1.php')
+}
+
+}
 ?>
