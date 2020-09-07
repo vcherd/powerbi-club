ec9eaaa6a85aefe46ea0f8887777ea9e8e7a3df0
suntod <67999195+suntod@users.noreply.github.com>
2020-09-07 07:43:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -336,9 +336,10 @@ if(!is_null($events)){
                                 'content' => http_build_query($data)
                             )
                         );
-                        /*
+                        
                         $context  = stream_context_create($options);
                         $result = file_get_contents($url, false, $context);
+                        /*
                         //if ($result === FALSE) { // Handle error  }
 
                         //var_dump($result);
