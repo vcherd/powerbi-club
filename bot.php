fef686cb7d0ad6ce048158e84f361679604dcca4
suntod <67999195+suntod@users.noreply.github.com>
2020-09-07 07:42:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -327,7 +327,7 @@ if(!is_null($events)){
                         
                         $url = 'https://bcpcheckin.bangchak.co.th/bcpcheckin/saveimage.php';
                         $data = array('key1' => 'value1', 'key2' => 'value2');
-                        /*
+                        
                         // use key 'http' even if you send the request to https://...
                         $options = array(
                             'http' => array(
@@ -336,6 +336,7 @@ if(!is_null($events)){
                                 'content' => http_build_query($data)
                             )
                         );
+                        /*
                         $context  = stream_context_create($options);
                         $result = file_get_contents($url, false, $context);
                         //if ($result === FALSE) { // Handle error  }
