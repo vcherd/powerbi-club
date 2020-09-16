3f742154b55c96ab9bfef5926564469dc6b8b0e6
suntod <67999195+suntod@users.noreply.github.com>
2020-09-16 16:37:50 +0700

    Update liff-starter.js

--- liff/app1/liff-starter.js
+++ liff/app1/liff-starter.js
@@ -1,9 +1,11 @@
+/// event เมื่อโหลดหน้าเพจเรียบร้อยแล้ว ให้ใช้คำสั่ง liff.init() สำหรับเตรียมความพร้อมใช้คำสั่ง LIFF ต่างๆ
 window.onload = function (e) {
-    liff.init(function (data) {
-        initializeApp(data);
+    liff.init(function (data) { // คำสั่ง init() คืนค่าข้อมูลของ LIFF app 
+        initializeApp(data); // เมื่อ liff พร้อมทำงาน เรียกฟังก์ชั่น initializeApp ส่งข้อมูล LIFF ไปแสดง
     });
 };
-
+ 
+// ฟังก์ชั่นสำหรับแสดงข้อมูล LIFF app และกำหนด การทำงานให้กับปุ่ม ต่างๆ 
 function initializeApp(data) {
     document.getElementById('languagefield').textContent = data.language;
     document.getElementById('viewtypefield').textContent = data.context.viewType;
@@ -11,19 +13,19 @@ function initializeApp(data) {
     document.getElementById('utouidfield').textContent = data.context.utouId;
     document.getElementById('roomidfield').textContent = data.context.roomId;
     document.getElementById('groupidfield').textContent = data.context.groupId;
-
+ 
     // openWindow call
     document.getElementById('openwindowbutton').addEventListener('click', function () {
         liff.openWindow({
             url: 'https://line.me'
         });
     });
-
+ 
     // closeWindow call
     document.getElementById('closewindowbutton').addEventListener('click', function () {
         liff.closeWindow();
     });
-
+ 
     // sendMessages call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
         liff.sendMessages([{
@@ -39,20 +41,20 @@ function initializeApp(data) {
             window.alert("Error sending message: " + error);
         });
     });
-
+ 
     // get access token
     document.getElementById('getaccesstoken').addEventListener('click', function () {
         const accessToken = liff.getAccessToken();
         document.getElementById('accesstokenfield').textContent = accessToken;
         toggleAccessToken();
     });
-
+ 
     // get profile call
     document.getElementById('getprofilebutton').addEventListener('click', function () {
         liff.getProfile().then(function (profile) {
             document.getElementById('useridprofilefield').textContent = profile.userId;
             document.getElementById('displaynamefield').textContent = profile.displayName;
-
+ 
             const profilePictureDiv = document.getElementById('profilepicturediv');
             if (profilePictureDiv.firstElementChild) {
                 profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
@@ -61,7 +63,7 @@ function initializeApp(data) {
             img.src = profile.pictureUrl;
             img.alt = "Profile Picture";
             profilePictureDiv.appendChild(img);
-
+ 
             document.getElementById('statusmessagefield').textContent = profile.statusMessage;
             toggleProfileData();
         }).catch(function (error) {
@@ -69,15 +71,18 @@ function initializeApp(data) {
         });
     });
 }
-
+ 
+//ฟังก์ชั่นสำหรับสลับซ่อนหรือแสดงค่า Access Token
 function toggleAccessToken() {
     toggleElement('accesstokendata');
 }
-
+ 
+//ฟังก์ชั่นสำหรับสลับซ่อนหรือแสดงข้อมูลบัญชีผู้ใช้
 function toggleProfileData() {
     toggleElement('profileinfo');
 }
-
+ 
+//ฟังกืชั่นสำหรับ สลับการซ่อนหรือแสดง html element ต่างๆ ตามค่าตัว element ที่ส่งมา
 function toggleElement(elementId) {
     const elem = document.getElementById(elementId);
     if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
