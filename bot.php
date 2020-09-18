9365b1205446ed29c3921a46b84969a8713967d6
suntod <67999195+suntod@users.noreply.github.com>
2020-09-18 15:05:23 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -332,7 +332,7 @@ if(!is_null($events)){
                                         )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
                                         'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
                                         substr_replace(date("Y-m-d H:i"),'T',10,1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
-                                        substr_replace(date("Y-m-d H:i",strtotime("+5 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
+                                        substr_replace(date("Y-m-d H:i",strtotime("+30 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
                                         substr_replace(date("Y-m-d H:i"),'T',10,1) //วันที่ เวลา น้อยสุดที่เลือกได้
                                     ),
                                     new UriTemplateActionBuilder(
