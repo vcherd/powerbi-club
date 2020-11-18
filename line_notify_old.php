1c13fd8d9e686d29bc7bf79d06b741d607de171a
suntod <67999195+suntod@users.noreply.github.com>
2020-11-18 17:06:56 +0700

    Create line_notify_old.php

---
+++ line_notify_old.php
@@ -0,0 +1,29 @@
+<?php
+$accToken = $_GET["token"];
+$message = $_GET["msg"];
+
+$notifyURL = "https://notify-api.line.me/api/notify";
+//$accToken = "xh1EjQw8A4E77Bu8Onc3xXtfxiYXhFSKofGz7LBDr62";
+$headers = array(
+ 'Content-Type: application/x-www-form-urlencoded',
+ 'Authorization: Bearer '.$accToken
+);
+
+$data = array('message' => $message); // enter message here
+
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, $notifyURL);
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2); 
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 1); 
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec( $ch );
+curl_close( $ch );
+ 
+var_dump($result);
+$result = json_decode($result,TRUE);
+?>
